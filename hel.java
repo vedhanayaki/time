f1452caf1e8f8d096b17f5c02c93a53d2fa2ede3
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-06 14:41:23 +0530

    Add files via upload

---
+++ hel.java
@@ -0,0 +1,14 @@
+package hel;
+
+public class hel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+for(int i=0;i<n;i++)
+{
+	System.out.println("hello");
+}
+	}
+
+}
